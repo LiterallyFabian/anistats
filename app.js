54fbb7585ac5413ea18c4ec74154abbbcb166ecf
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-07-03 00:33:24 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: false, // true = .sass and false = .scss
+    sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const router = express.Router();
+const graphqlUrl = 'https://graphql.anilist.co';
+let cache = {};
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Anistats'});
+});
+
+router.post('/stats', function (req, res) {
+    let username = req.body.username;
+    if (username.includes("https://anilist.co/user/"))
+        username = username.split("https://anilist.co/user/")[1].split("/")[0];
+
+    // Check for cached data
+    if (cache[username]) {
+        if(cache[username].time + 60 * 60 * 1000 > Date.now()) {
+            // Return cached data if it's less than an hour old
+            return res.send(cache[username].data);
+        }
+    }
+
+    // language=GraphQL
+    let query = `query ($username: String) {
+    User(name: $username) {
+        id
+        name
+        avatar {
+            large
+        }
+        createdAt
+        mediaListOptions {
+            scoreFormat
+        }
+        bannerImage
+        statistics {
+            anime {
+                count
+                meanScore
+                minutesWatched
+                episodesWatched
+                genrePreview: genres(sort: COUNT_DESC){
+                    genre
+                    count
+                    meanScore
+                }
+            }
+        }
+        favourites{
+            anime{
+                edges {
+                    node{
+                        id
+                        type
+                        isAdult
+                        bannerImage
+                        coverImage {
+                            large
+                        }
+                        title{
+                            english
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+    `
+
+    let variables = {
+        username: username
+    }
+
+
+    console.log(`Getting stats for ${username}`);
+    fetch(graphqlUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+                query: query,
+                variables: variables
+            }
+        )
+    }).then(r => r.json()).then(data => {
+        res.send(data);
+    });
+
+
+});
+
+module.exports = router;
