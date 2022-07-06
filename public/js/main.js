4babb8e533120c8eb9511d2908f99e3b219ee259
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-07-06 12:25:54 +0200

    Add filter buttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+/**
+ * Redirects the user to /stats/:username, where :username is the username of the user entered in the form.
+ */
 function redirect() {
     // get value from #username field
 
@@ -8,3 +11,33 @@ function redirect() {
     // redirect user to /stats/:username
     window.location.href = "/stats/" + username;
 }
+
+/**
+ * Sets the currently visible tab to the tab with the given id. If ID is all, all tabs are shown.
+ * @param ID of the tab to set as visible
+ */
+function setWindow(ID) {
+    console.log("Setting window to " + ID);
+    $("[id*=li\-]").removeClass("active");
+
+    if (ID !== 'all') {
+        $(".stats-row").hide();
+        $("#row-" + ID).show();
+        $('#li-' + ID).addClass("active");
+    } else {
+        $(".stats-row").show();
+        $("#li-all").addClass("active");
+    }
+}
+
+$(document).ready(function () {
+    // set default window to all
+    setWindow('all');
+
+    // set up click handlers for tabs
+    $("[id*=li\-]").click(function () {
+            let ID = $(this).attr("id").split("-")[1];
+            setWindow(ID);
+        }
+    );
+});
